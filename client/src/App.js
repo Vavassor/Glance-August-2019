a8284ab19951ef83c06652fcca6cbd653725196f
Andrew Dawson <dawso.andrew@gmail.com>
2019-07-25 20:15:24 -0400

    Adds routing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
 import React from "react";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 
-function App() {
+export default function App(props) {
   return (
-    <header>
-      <h1>Howdy!</h1>
-    </header>
+    <Router>
+      <header>
+        <nav>
+          <Link to="/">Dashboard</Link>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/about" component={About} />
+    </Router>
   );
 }
-
-export default App;
